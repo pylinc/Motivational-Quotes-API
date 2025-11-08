aacf08c370f0e01bdf9531fb450609bbc79270f6
Kunal Rathore <kunalrathorework@gmail.com>
2025-11-09 00:47:00 +0530

    added homepage route

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ Get all quotes
+app.get("/", (req, res) => {
+  res.send(`
+    <div style="font-family: Arial; text-align: center; margin-top: 100px;">
+      <h1>🚀 Welcome to the Motivational Quotes API</h1>
+      <p>✨ Your daily dose of inspiration for students and dreamers!</p>
+      <p>👉 <a href="/api/quotes/random">Click here to get a random quote</a></p>
+      <p>📚 Or visit <a href="/api/quotes">/api/quotes</a> to view all quotes</p>
+    </div>
+  `);
+});
+
 app.get("/api/quotes", (req, res) => {
   res.json(quotes);
 });
